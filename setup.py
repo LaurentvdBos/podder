a2a1351f74aa329f455c50bf61b2895792606f0e
Laurent van den Bos <laurentvdbos@outlook.com>
2023-08-04 12:00:17 +0200

    Make the package buildable

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+
+setup(name="podder",
+      version="0.0.1",
+      author="Laurent van den Bos",
+      author_email="laurentvdbos@outlook.com",
+      license="MIT",
+      packages=find_packages(include=['podder']))
