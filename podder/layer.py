14acf174c05055325e3015277febf93e99542c58
Laurent van den Bos <laurentvdbos@outlook.com>
2023-12-15 21:12:20 +0100

    Revert "Execute dhclient in namespace"
    This reverts commit 9e121a5177744ea5cb4ba647885c8b26337f45b0.

--- podder/layer.py
+++ podder/layer.py
@@ -391,13 +391,12 @@ class Layer:
 
         # Add bind mounts to configure network; we do this before the pivot root
         # such that symlinks resolve correctly.
-        if self.ifname is not None:
-            for what in ("/etc/hosts", "/etc/hostname", "/etc/resolv.conf"):
-                # Ensure the file exists
-                open(os.path.join(self.path, "merged", what[1:]), mode='w').close()
+        for what in ("/etc/hosts", "/etc/hostname", "/etc/resolv.conf"):
+            # Ensure the file exists
+            open(os.path.join(self.path, "merged", what[1:]), mode='w').close()
 
-                # Do the bind mount, resolving any symlinks the files may have
-                linux.mount(os.path.realpath(what), os.path.join(self.path, "merged", what[1:]), "ignored", linux.MS_BIND, None)
+            # Do the bind mount, resolving any symlinks the files may have
+            linux.mount(os.path.realpath(what), os.path.join(self.path, "merged", what[1:]), "ignored", linux.MS_BIND, None)
 
         os.mkdir(os.path.join(self.path, "merged", "old_root"))
         try:
